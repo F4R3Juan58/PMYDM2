1afcf554c25b959491416aa0c92052596d4175a1
F4R3 <96112394+F4R3Juan58@users.noreply.github.com>
2025-02-25 16:34:21 +0100

    Create MainActivity4.java

---
+++ 21-01-2025/MainActivity4.java
@@ -0,0 +1,8 @@
+public class ThirdFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Infla el diseño del tercer fragmento
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+}
